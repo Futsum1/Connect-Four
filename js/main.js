e52a4660f6d991187959f46631c77ca321b05590
Futsum Nemariam <futsumnemariam2@gmil.com>
2020-08-12 15:29:08 -0700

    projec1

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ let gameBoard, playerTurn, winner;
 // cache elements
 const circles = document.querySelectorAll("td");
 const message = document.querySelector("h1 span");
+//const msg = document.getElementById('won');
 
 //Event Listeners
 document.querySelector("table").addEventListener("click", updateBoard);
@@ -18,22 +19,50 @@ document.querySelector("#reset-button").addEventListener("click", initialize);
 function initialize() {
     gameBoard = new Array(36).fill(null);
     playerTurn = 1;
+    render();
+    
 } 
-
+function render(){
+    gameBoard.forEach(function (cirlce, idx) {
+    circles[idx].style.backgroundColor = lookup[cirlce];
+    
+    
+    
+})
+}
 function updateBoard(event) {
     let index = parseInt(event.target.id.replace("c", ""));
+    if (horizontalWinner()){ 
+        //return msg.innerText = "winner"}
     if (gameBoard[index]) {
-        return;
+       // return;
     }
     while(!gameBoard[index] && index < 36) {
         index += 6;
     }
     index -= 6;
     gameBoard[index] = playerTurn;
+    console.log(horizontalWinner());
     message.innerHTML = lookup[playerTurn]
     playerTurn *= -1;
-    circles[index].style.backgroundColor = lookup[playerTurn];
+    circles[index].style.backgroundColor = lookup[playerTurn]
+    console.log(gameBoard);
+    //gameBoard.forEach(fucntion(checkForWinner))
 }
+
+
+
+    function verticalWinner(circleToCheck) {
+        // Check if index + (index + 1) + (index + 1) + (index + 1) === 4
+      }
+
+
+ function horizontalWinner(){
+     return gameBoard.some(function(element, idx) {
+         return Math.abs(gameBoard[idx] + gameBoard[idx +1] + gameBoard[idx +2] + gameBoard[idx +3] === 4 && idx % 6 < 3)
+     })
+ }
+// check if index + (1 row down) + (1 row down) + (1 row down) === 4
 
 initialize();
 

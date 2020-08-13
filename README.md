11a9329905d2f052d68ea557bafee9053f9dbd97
Futsum Nemariam <futsumnemariam2@gmil.com>
2020-08-13 16:50:14 -0700

    final

--- README.md
+++ README.md
@@ -1,81 +1,12 @@
-# Project-1 
+# Connect Four 
+<img src="./image1.png"/> 
 
-Model-view-controller
-
-View - html
-Model - logic (js)
-Controller (bridge)
-
-==========================================================================
-
-Model
-Win condition
-Tie condition (play again)
-Player’s turn
-Manage game state
-
-Data
-Const gameBoard = an array of 6x6 with all elements set to 0 (blank state)
-Const player1 = 1 
-Const player2 = 2
-Const playerTurn = player1
-
-Function getPlayerTurn() {
- return playerTurn
-}
-
-Function updatePlayerTurn(previousPlayer) {
-	if previousPlayer is equal to player1 return player2. Else  return player1
-}
-
-Function updateBoard(player, x, y) {
-	for (I=boardGame.length - 1; I >= 0 ; I++) {
-		if boardGame[I][y] is 0
-			boardGame[I][y] = player
-			break
-	}
-}
-
-Function checkWinCondition(player) {
-	// horizontal check
-	// vertical check
-	// diagonal check
-}
-	
-===================================================================
-
-
-
-
-const horizontalCount = 0
-	for(I=0; I<boardGame.length; I++) {
-		const y=0
-		while (y < 5) {
-			if board[I][j] is equal to player {
-			horizontalCount += 1
-			y += 1
-		}
-	}
-
-	}
-}
-
-
-
-
-0  	0	1  	1 	0  	0
-
-
-0  	0	0  	0	0  	0
-
-
-0  	0	0  	0 	0  	0
-
-
-0  	0	0  	0	0  	0
-
-
-2  	0	0  	0	0  	0
-
-
-1 	0	1  	0	0  	0
+## Game Play 
+This game play by two its really good enjoy it 
+## The Language 
+HTML CSS Javascript 
+## Getting Started 
+Link to game https://futsum1.github.io/Project-1/
+## Next Step 
+ I use CSS to design logic Javascript and all the messages are HTML 
+ 
--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let gameBoard, playerTurn, winner;
 // cache elements
 const circles = document.querySelectorAll("td");
 const message = document.querySelector("h1 span");
-//const msg = document.getElementById('won');
+const msgWin = document.getElementById('won')
 
 //Event Listeners
 document.querySelector("table").addEventListener("click", updateBoard);
@@ -19,50 +19,49 @@ document.querySelector("#reset-button").addEventListener("click", initialize);
 function initialize() {
     gameBoard = new Array(36).fill(null);
     playerTurn = 1;
+    winner = false;
+    msgWin.innerText = null;
     render();
-    
 } 
+
 function render(){
+    //gameBoard.forEach(function (cirlce, idx) {
+       // circles[idx].style.backgroundColor = lookup[cirlce];
+    if (winner) {
+        msgWin.innerText =  lookup[playerTurn] + " won ";
+    } else {
+        message.innerHTML = lookup[playerTurn] 
+    }
     gameBoard.forEach(function (cirlce, idx) {
-    circles[idx].style.backgroundColor = lookup[cirlce];
-    
-    
-    
-})
+        circles[idx].style.backgroundColor = lookup[cirlce];
+    })
 }
+
 function updateBoard(event) {
     let index = parseInt(event.target.id.replace("c", ""));
-    if (horizontalWinner()){ 
-        //return msg.innerText = "winner"}
-    if (gameBoard[index]) {
-       // return;
+    if (gameBoard[index] || winner) {
+        return;
     }
     while(!gameBoard[index] && index < 36) {
         index += 6;
     }
     index -= 6;
     gameBoard[index] = playerTurn;
-    console.log(horizontalWinner());
-    message.innerHTML = lookup[playerTurn]
+    winner = checkWinner();
+    render();
     playerTurn *= -1;
-    circles[index].style.backgroundColor = lookup[playerTurn]
-    console.log(gameBoard);
-    //gameBoard.forEach(fucntion(checkForWinner))
 }
 
-
-
-    function verticalWinner(circleToCheck) {
-        // Check if index + (index + 1) + (index + 1) + (index + 1) === 4
-      }
-
-
- function horizontalWinner(){
+ function checkWinner(){
      return gameBoard.some(function(element, idx) {
-         return Math.abs(gameBoard[idx] + gameBoard[idx +1] + gameBoard[idx +2] + gameBoard[idx +3] === 4 && idx % 6 < 3)
+         // horizontal
+         return (idx % 6 < 3 && Math.abs(gameBoard[idx] + gameBoard[idx + 1] + gameBoard[idx + 2] + gameBoard[idx + 3]) === 4) ||
+         // ver
+         (idx < 18 && Math.abs(gameBoard[idx] + gameBoard[idx + 6] + gameBoard[idx + 12] + gameBoard[idx + 18]) === 4) ||
+         (idx % 6 < 3 && Math.abs(gameBoard[idx] + gameBoard[idx + 7] + gameBoard[idx + 14] + gameBoard[idx + 21]) === 4) ||
+         (idx % 6 > 2 && idx < 18 && Math.abs(gameBoard[idx] + gameBoard[idx + 5] + gameBoard[idx + 10] + gameBoard[idx + 15]) === 4)
      })
  }
-// check if index + (1 row down) + (1 row down) + (1 row down) === 4
 
 initialize();
 
